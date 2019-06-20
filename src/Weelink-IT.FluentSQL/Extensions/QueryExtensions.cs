fea4847939731b07d101b4aaf1759668d4042d1e
Jeroen Weelink <github@weelink-it.nl>
2019-06-20 12:36:00 +0200

    Make executing a query an extension method on IDbConnection. It could make integration with e.g. Dapper easier

---
+++ src/Weelink-IT.FluentSQL/Extensions/QueryExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace WeelinkIT.FluentSQL.Extensions
+{
+    /// <summary>
+    ///     Extends <see cref="IDbConnection" /> for executing queries.
+    /// </summary>
+    public static class QueryExtensions
+    {
+        /// <summary>
+        ///     Execute this query and return the result.
+        /// </summary>
+        /// <param name="connection">The <see cref="IDbConnection" />.</param>
+        /// <param name="query">The query to execute.</param>
+        /// <param name="parameterConfig">Configures the parameters.</param>
+        /// <param name="transaction">The optional transaction.</param>
+        /// <returns>The result of the query.</returns>
+        public static Task<TQueryResult> ExecuteAsync<TParameters, TQueryResult>(
+            this IDbConnection connection,
+            Query<TParameters, TQueryResult> query,
+            Action<TParameters> parameterConfig,
+            IDbTransaction transaction = null) where TParameters : new()
+        {
+            var parameters = new TParameters();
+            parameterConfig(parameters);
+
+            return Task.FromResult(default(TQueryResult));
+        }
+
+        /// <summary>
+        ///     Execute this query and return the result.
+        /// </summary>
+        /// <param name="connection">The <see cref="IDbConnection" />.</param>
+        /// <param name="query">The query to execute.</param>
+        /// <param name="transaction">The optional transaction.</param>
+        /// <returns>The result of the query.</returns>
+        public static Task<TQueryResult> ExecuteAsync<TQueryResult>(
+            this IDbConnection connection,
+            Query<TQueryResult> query,
+            IDbTransaction transaction = null)
+        {
+            return Task.FromResult(default(TQueryResult));
+        }
+    }
+}
